f193f9da76f855a0dd54c8036de53febc13c263e
AasmundEndresen <a.b.endresen@gmail.com>
2021-01-24 08:10:08 +0100

    ABE: Added new api requests

--- src/services/market.services.js
+++ src/services/market.services.js
@@ -10,3 +10,22 @@ export const getMarketData = async function () {
     console.log(err);
   }
 }
+
+export const getCoinData = async function (id) {
+  try {
+    const options = `vs_currency=usd&ids=[${id}]&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+    const response = await axios.get(`${api}/coins/markets?${options}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const getTrendingCoins = async function () {
+  try {
+    const response = await axios.get(`${api}/search/trending`);
+    return response.coins.map(({ item }) => item);
+  } catch (err) {
+    console.log(err);
+  }
+}
