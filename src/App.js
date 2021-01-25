613e20992c4d20bd2e2ac14beb2a620f5c60fa4b
AasmundEndresen <a.b.endresen@gmail.com>
2021-01-25 23:33:50 +0100

    ABE: Added donations per request

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { PriceProvider } from './context/priceContext'
 import MainTable from './components/MainTable'
 import TrendingTable from './components/TrendingTable'
 import CoinSelect from './components/CoinSelect'
+import { ReactComponent as Btc } from './components/btc.svg'
+import { ReactComponent as Xlm } from './components/btc.svg'
+import { ReactComponent as Eth } from './components/btc.svg'
 
 function App({ className }) {
   const [component, setComponent] = useState(MainTable);
@@ -24,6 +27,21 @@ function App({ className }) {
             <CoinSelect />
           </div>
         </nav>
+        <div class="donations">
+          <h2>Addresses for donations</h2>
+          <div>
+            <span className="asset">BTC</span>
+            <Btc className="qr" />
+          </div>
+          <div>
+            <span className="asset">ETH/ERC20</span>
+            <Eth className="qr" />
+          </div>
+          <div>
+            <span className="asset">XLM</span>
+            <Xlm className="qr" />
+          </div>
+        </div>
       </header>
       {component}
     </PriceProvider>
@@ -34,6 +52,41 @@ export default styled(App)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  .donations {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-bottom: 12px;
+    h2 {
+      margin: 0;
+      padding: 0;
+      line-height: 1;
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      mix-blend-mode: soft-light;
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 12px;
+    }
+    .asset {
+      font-size: 14px;
+      font-weight: bold;
+      padding: 0;
+      margin: 0;
+      color: blue;
+      mix-blend-mode: soft-light;
+    }
+    .qr {
+      width: 100px;
+      mix-blend-mode: soft-light;
+    }
+  }
   .nav {
     box-sizing: border-box;
     width: 100%;
@@ -43,7 +96,6 @@ export default styled(App)`
     justify-content: space-between;
     padding: 12px 32px;
     align-items: center;
-    margin-bottom: 24px;
     @media (max-width: 420px) {
       justify-content: center;
     }
