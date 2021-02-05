7e684a8d96535b9fdaa1f71738f7c2c7911dda47
AasmundEndresen <a.b.endresen@gmail.com>
2021-02-05 12:18:22 +0100

    ABE: Put Selectedlist as home component

--- src/app.routes.js
+++ src/app.routes.js
@@ -3,6 +3,7 @@ import { TrendingTable, SelectedTable } from './components';
 const appRoutes = [
   { path: '/selected', title: 'Selected', component: SelectedTable },
   { path: '/trending', title: 'Trending', component: TrendingTable },
+  { path: '/', title: 'Home', component: SelectedTable },
 ];
 
 export default appRoutes;
